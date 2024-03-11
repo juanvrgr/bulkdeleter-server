85749cca0069431983e3da01b946584e4fc92f66
juanvrgr <jnmvrgr@gmail.com>
2024-03-11 09:31:50 -0300

    Added plan amount validation

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -3,12 +3,25 @@ require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const router = express.Router();
 
+const planPrices = {
+  1: { amount: 2000 },
+  2: { amount: 4000 }
+}
+
 router.post('/create-intent', async (req, res) => {
   try {
-    const { amount } = req.body;
+    const { currency, planId } = req.body;
+
+    const expectedAmount = planPrices[planId].amount;
+    const clientAmount = req.body.amount;
+
+    if (expectedAmount !== clientAmount) {
+      return res.status(400).json({ error: 'Invalid amount' });
+    }
+
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100,
-      currency: 'usd',
+      amount: planPrices[planId].amount,
+      currency: currency,
     });
 
     res.status(200).json({ clientSecret: paymentIntent.client_secret });
